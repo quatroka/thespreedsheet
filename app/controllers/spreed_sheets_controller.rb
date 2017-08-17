061c7d9bedbfa77356e5afb489aec834c81208a2
quatroka <monteiro.leonardosantos@gmail.com>
2017-08-17 04:47:26 -0300

    finish work

--- app/controllers/spreed_sheets_controller.rb
+++ app/controllers/spreed_sheets_controller.rb
@@ -3,11 +3,18 @@ class SpreedSheetsController < ApplicationController
   before_action :current_user
 
   def index
+    @spreedsheets = spreedsheets
     render layout: 'dashboard'
   end
 
   def import
     SpreedSheet.import(params[:file])
     redirect_to spreedsheets_path, notice: 'Data imported.'
+  end
+
+  private
+
+  def spreedsheets
+    SpreedSheet.where(user_id: @current_user)
   end
 end
